42a2786c667f891e32f834155c25f438b45581f3
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2024-10-27 17:43:52 +0100

    chore: commenting out og since its available in canary only

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,4 +1,4 @@
-import { ImageResponse } from 'next/og'
+/*import { ImageResponse } from 'next/og'
 
 export function GET(request: Request) {
   let url = new URL(request.url)
@@ -20,3 +20,4 @@ export function GET(request: Request) {
     }
   )
 }
+*/
