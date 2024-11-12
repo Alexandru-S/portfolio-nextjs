52be35e2389dc0097f2866a1d12991c684c57bd7
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2024-11-13 00:05:02 +0100

    feat: changed badges to neutral

--- app/components/RepoCard.tsx
+++ app/components/RepoCard.tsx
@@ -1,35 +1,34 @@
 interface Repo {
-    id: number;
-    name: string;
-    language: string;
-    clone_url: string;
-    updated_at: string;
-  }
-  
-  interface RepoCardProps {
-    repo: Repo;
-  }
-  
-  export function RepoCard({ repo }: RepoCardProps) {
-    return (
-      <div className="card bg-base-100 shadow-lg bg-diagonal-lines">
-        <div className="card-body">
-          <h3 className="card-title text-xl font-semibold">{repo.name}</h3>
-          <div className="card-actions">
-            <div className="badge badge-outline">{repo.language ? `Language: ${repo.language}` : 'No language specified'}</div>
-          </div>
-          <div className="card-actions">
-            <a
-              href={repo.clone_url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="btn btn-primary text-white underline"
-            >
-              Visit Repository
-            </a>
-          </div>
+  id: number;
+  name: string;
+  language: string;
+  clone_url: string;
+  updated_at: string;
+}
+
+interface RepoCardProps {
+  repo: Repo;
+}
+
+export function RepoCard({ repo }: RepoCardProps) {
+  return (
+    <div className="card bg-base-100 shadow-lg bg-diagonal-lines">
+      <div className="card-body">
+        <h3 className="card-title text-xl font-semibold">{repo.name}</h3>
+        <div className="card-actions">
+          <div className="badge badge-neutral">{repo.language ? `${repo.language}` : 'No language specified'}</div>
+        </div>
+        <div className="card-actions">
+          <a
+            href={repo.clone_url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-primary text-white underline"
+          >
+            Visit Repository
+          </a>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
