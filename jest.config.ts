6363656dfa24d09252a2f388bb10a4516827860e
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2024-11-21 22:57:51 +0100

    chore: in progress configuring jest test

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/configuration
  */
-
 import type {Config} from 'jest';
 
 const config: Config = {
@@ -138,6 +137,7 @@ const config: Config = {
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ["./jest.setup.ts"],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -176,6 +176,9 @@ const config: Config = {
 
   // A map from regular expressions to paths to transformers
   // transform: undefined,
+  transform: {
+    "^.+\\.tsx?$": "ts-jest", // Use ts-jest to transform TypeScript files
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
