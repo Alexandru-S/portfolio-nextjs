2ea185366698032a7cffbd723db280aae0de9e32
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2025-02-23 17:29:32 +0100

    feat: added blog page layout

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -59,7 +59,7 @@ export default function Blog({ params }) {
   }
 
   return (
-    <section>
+    <section className="container mx-auto p-4">
       <script
         type="application/ld+json"
         suppressHydrationWarning
