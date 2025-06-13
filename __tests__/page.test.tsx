c7bd9aef9eb94bf3b0cf994598fcb9faba0a28e0
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2025-06-13 23:45:02 +0200

    Update page.test.tsx

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,28 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import Page from '../app/page';
+import { mockRepos } from './test.consts.ts';
 
 beforeEach(() => {
   fetchMock.resetMocks();
 });
 
 test('fetches and displays repos', async () => {
-  const mockRepos = [
-    {
-      id: 1,
-      name: 'Repo 1',
-      language: 'TypeScript',
-      clone_url: 'https://example.com/repo1',
-      updated_at: '2024-01-01T12:00:00Z',
-    },
-    {
-      id: 2,
-      name: 'Repo 2',
-      language: 'JavaScript',
-      clone_url: 'https://example.com/repo2',
-      updated_at: '2024-01-02T12:00:00Z',
-    },
-  ];
-
   fetchMock.mockResponseOnce(JSON.stringify(mockRepos));
   render(<Page />);
 
