9f79e05133e8bbb315ed4c6463149ba77ffc5bf6
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2025-06-13 23:40:43 +0200

    Update test.consts.ts

--- __tests__/test.consts.ts
+++ __tests__/test.consts.ts
@@ -1,1 +1,16 @@
-
+  export const mockRepos = [
+    {
+      id: 1,
+      name: 'Repo 1',
+      language: 'TypeScript',
+      clone_url: 'https://example.com/repo1',
+      updated_at: '2024-01-01T12:00:00Z',
+    },
+    {
+      id: 2,
+      name: 'Repo 2',
+      language: 'JavaScript',
+      clone_url: 'https://example.com/repo2',
+      updated_at: '2024-01-02T12:00:00Z',
+    },
+  ];
